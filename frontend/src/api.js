ef51f9be31930ff6bc14591893ef5ba80d9e8df6
sj-15 <15.sj.sourav@gmail.com>
2025-11-09 18:46:51 +0530

    fixed api issue

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,14 +1,13 @@
 import axios from 'axios'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
 })
-
+console.log("API Base URL:", API_BASE_URL);
 // fetch all expenses
 export const fetchExpenses = () => api.get('/expenses').then(r => r.data)
 
